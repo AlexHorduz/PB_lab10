a73b72e73f12817644dd89b18f4d44272b38c6d7
AlexHorduz <74256406+AlexHorduz@users.noreply.github.com>
2021-04-14 19:51:22 +0300

    Update Actor.java

--- Actor.java
+++ Actor.java
@@ -3,23 +3,50 @@ package lab10_PB_Horduz_Sachko_Sachko;
 import java.util.ArrayList;
 
 public class Actor {
-	private final String name;
-	private ArrayList<String> films;
-	
-	public Actor(String name, ArrayList<String> films) {
-		this.name = name;
-		this.films = films;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	
-	public ArrayList<String> getFilms() {
-		return (ArrayList<String>) this.films.clone();
-	}
-	
-	public void addFilm(String film) {
-		this.films.add(film);
-	}
+    private final String name;
+    private ArrayList<Film> films;
+
+    public Actor(String name) {
+        this.name = name;
+        this.films = new ArrayList<Film>();
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Film> getFilms() {
+        return (ArrayList<Film>) this.films.clone();
+    }
+
+    public void addFilm(Film film) {
+        this.films.add(film);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 17;
+        return (prime + name.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
+
+        Actor other = (Actor) o;
+
+        return other.hashCode() == hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 }

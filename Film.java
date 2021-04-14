a8a7cbea79b24865d8fdd4eea4ec23f31853ffde
AlexHorduz <74256406+AlexHorduz@users.noreply.github.com>
2021-04-14 19:50:24 +0300

    Update Film.java

--- Film.java
+++ Film.java
@@ -3,19 +3,48 @@ package lab10_PB_Horduz_Sachko_Sachko;
 import java.util.ArrayList;
 
 public class Film {
-	private final String name;
-	private final ArrayList<String> actors;
-	
-	public Film(String name, ArrayList<String> actors) {
-		this.name = name;
-		this.actors = actors;
-	}
-	
-	public String getName() {
-		return this.name;
-	}
-	
-	public ArrayList<String> getActors() {
-		return (ArrayList<String>) this.actors.clone();
-	}
+    private final String name;
+    private final ArrayList<Actor> actors;
+
+    public Film(String name, ArrayList<Actor> actors) {
+        this.name = name;
+        this.actors = actors;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public ArrayList<Actor> getActors() {
+        return (ArrayList<Actor>) this.actors.clone();
+    }
+
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 17;
+        return (prime + name.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()){
+            return false;
+        }
+
+        Film other = (Film) o;
+
+        return other.hashCode() == hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "\"" + this.name +  "\"" + " Actors: " + this.actors.toString();
+    }
+
 }
